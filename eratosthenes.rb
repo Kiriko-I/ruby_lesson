203747a2007e3572d916168a2a8def7bde6ce580
Kiriko-I <kirikotrombone@gmail.com>
2023-06-22 18:44:46 +0900

    [Add]eratosthenes.rb修正

--- eratosthenes.rb
+++ eratosthenes.rb
@@ -1,20 +1,24 @@
 def eratosthenes(n)
     is_prime = ["YES"] * (n+1)
     is_prime[0], is_prime[1] = "NO", "NO"
-    primes = []
+    # primes = [] nまでの素数をすべて格納する配列
     sq = (n**0.5+1).to_i
-
+    
     (2..sq).each do |i|
         if is_prime[i] == "YES"
-            primes << i
-            j = i * i
+            # primes << i
+            j = i * i #二乗するまでの数は、その数の前までの素数で検討済みのため。e.g. i=5のとき、10はi=2で検討している。
             while j <= n+1
                 is_prime[j] ="NO"
-                j += i
+                j += i # iの倍数を検討
             end
         end
     end
-    return primes
+    if is_prime[n] == "NO"
+        return result = "NO"
+    else
+        return result = "YES"
+    end
 end
 n = gets.to_i
 puts(eratosthenes(n))
