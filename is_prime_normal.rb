cf8bb4e956ce8168ce5ed4c2aed60ebb1c5b6b25
Kiriko-I <kirikotrombone@gmail.com>
2023-06-23 09:55:57 +0900

    [Add]each文に修正

--- is_prime_normal.rb
+++ is_prime_normal.rb
@@ -3,7 +3,7 @@ is_prime = "YES"
 if n == 1
     is_prime = "NO"
 else
-    for i in 2..(n**0.5 + 1) do
+    (2..(n**0.5 + 1)).each do |i| # for i in 2..(n**0.5 + 1) do
         result = n % i
         if result == 0
             is_prime = "NO"
