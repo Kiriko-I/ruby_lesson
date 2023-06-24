81663d17e040f75a0732a22ff4b944792af5c099
Kiriko-I <kirikotrombone@gmail.com>
2023-06-24 13:34:07 +0900

    [Add]is_prime_multi.rb追加

---
+++ is_prime_multi.rb
@@ -0,0 +1,29 @@
+# Rubyは大文字小文字を区別する
+# 大文字は定数、小文字は変数
+
+def eratosthenes(n)
+    is_prime = ["YES"] * (n+1)
+    is_prime[0], is_prime[1] = "NO", "NO"
+    sq = (n**0.5+1).to_i
+    
+    (2..sq).each do |i|
+        if is_prime[i] == "YES"
+            j = i * i
+            while j <= n+1
+                is_prime[j] ="NO"
+                j += i
+            end
+        end
+    end
+    if is_prime[n] == "YES"
+        puts("pass")
+    else
+        puts("failure")
+    end
+end
+
+N = gets.to_i
+(1..N).each do
+    a = gets.to_i
+    eratosthenes(a) == "NO"
+end
