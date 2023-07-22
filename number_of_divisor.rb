7021db6330f0b76c67a879c6593c66de3d6a7f3a
Kiriko-I <kirikotrombone@gmail.com>
2023-07-22 12:45:02 +0900

    [Add]number_of_divisor.rbひとまず回答出した

--- number_of_divisor.rb
+++ number_of_divisor.rb
@@ -13,22 +13,24 @@ while i <= N do
 end
 
 j = 0
-a = divisor[j]
-b = divisor[j+1]
+# a = divisor[j]
+# b = divisor[j+1]
+# このaとbを使うと、jが0で固定されてしまう
 num = []
 count = 1
 
 while j+2 <= divisor.size do
-    if a != b
-        num.push(count)
-        count = 1
-    else
+    if divisor[j] == divisor[j+1]
         count += 1
-    end
-    if j+2 == divisor.size && a == b
-        num.push(count)
+    else
+        num.push(count+1)
+        count = 1
     end
     j += 1
 end
-# puts(num)
-# puts(num.inject(:*))
+if N == 1
+    num.push(1)
+else
+    num.push(count+1) # 最後に入力されていたカウント数を追加
+end
+puts(num.inject(:*))
